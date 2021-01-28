adf839d7d2345f325859bb5b70e3b3caadd51b74
Guðmundur Snorri Sigfússon <gummehh@gmail.com>
2021-01-28 12:08:52 +0000

    Delete backend directory

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'd75428e5f872b7114e6985ad2135e76e'),
-    },
-  },
-})
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '4e992d5c-596d-46b9-9270-c329a12e9311',
-}
