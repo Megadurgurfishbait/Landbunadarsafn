83c60649149dec57b01d0885414e3853a02405a9
Guðmundur Snorri Sigfússon <gummehh@gmail.com>
2021-01-28 14:02:50 +0000

    log path to strapi

--- src/lib/api/getStrapiURL.js
+++ src/lib/api/getStrapiURL.js
@@ -2,7 +2,7 @@ import { request } from 'http'
 
 export function getStrapiURL(path = '') {
   return `${
-    process.env.NODE_ENV.STRAPI_API_URL ||
+    process.env.STRAPI_API_URL ||
     process.env.NEXT_PUBLIC_STRAPI_API_URL ||
     'http://localhost:1337'
   }${path}`
@@ -10,9 +10,9 @@ export function getStrapiURL(path = '') {
 
 // Helper to make GET requests to Strapi
 export async function fetchAPI(path) {
+  console.log(path)
   if (path === '/articles/undefined') return null
   const requestUrl = getStrapiURL(path)
-  console.log(requestUrl)
   const response = await fetch(requestUrl)
   const data = await response.json()
   return data
